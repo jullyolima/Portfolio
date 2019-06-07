8e4288a9a495991622611ca8b580bff753f32201
unknown <jullyolima@hotmail.com>
2019-06-07 12:19:20 -0700

    setup components and index

---
+++ client/src/components/main/main.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./main.css"
+import Main from './components/main/main'
+
+
+class Main extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        
+      }
+    }
+  
+  
+    render() {
+      
+      
+    }
+
+
+
+
+}
+
+
+  export default Main;
